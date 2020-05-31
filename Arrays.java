611d25dea6b5e5fbff0cb6656899a38e0a945d76
ESRA KURT <47241960+esrkrt@users.noreply.github.com>
2020-05-31 13:13:13 +0300

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,97 @@
+
+package arrays;
+
+
+/*
+
+
+        public class Arrays {
+
+    public static void main(String[] args) {
+         int[]arr1={10,20,30,40,50};
+         int[]arr2=new int[5];
+         arr2[0]=1;
+         arr2[1]=2;
+           arr2[2]=3;
+         arr2[3]=4;
+           arr2[4]=5;
+              System.out.println("array2 nin birinci elemanı..."+arr2[0]);
+               System.out.println("array2 nin ikinci elemanı..."+arr2[1]);
+                System.out.println("array1 in üçüncü elemanı ..."+arr1[2]);
+    }
+    
+    
+}
+
+public class Arrays {
+
+    public static void main(String[] args) {
+         int[]arr1={10,20,30,40,50};
+         for(int i=0;i<arr1.length;i++){ 
+             System.out.println(" eleman ..."+arr1[i]);
+             
+    }
+         
+    }
+}
+
+import java.util.Scanner; 
+public class Arrays {
+
+    public static void main(String[] args) {
+        
+    Scanner scan=new Scanner(System.in);
+          int []arr2=new int[5] ;
+           System.out.println(" arry2 nin degerini girin...");
+           for(int i=0;i<arr2.length;i++){
+                arr2[i]=scan.nextInt();
+    }
+           System.out.println("array2 nin degerleri yazılıyor...");
+           for(int i=0;i<arr2.length;i++){ 
+                System.out.println(" eleman ..."+arr2[i]);
+           }
+           
+    }
+}
+
+public class Arrays {
+
+    public static void main(String[] args) {   
+    int toplam=0;
+    int []arr1={1,2,3,4,5,6};
+    for(int i=0;i<arr1.length;i++){
+        
+       toplam+=arr1[i];
+       
+        }
+    
+          System.out.println(" ortalama ;"+((double)toplam)/(arr1.length));
+          
+          
+        
+    }
+}       
+*/
+public class Arrays {
+
+    public static void ortalama(int[] arr) {   
+    int toplam=0;
+    for(int i=0;i<arr.length;i++){
+        
+       toplam+=arr[i];
+       
+        }
+    
+          System.out.println(" ortalama ;"+((double)toplam)/(arr1.length)); 
+    }
+}
+  public static void main (String []args) {
+     scanner scan =new scanner (system.in); 
+    int []arr2=new int[5];
+    System.out.println("arrayın degerlerini girin:");
+    for(int i=0;i<arr2.length;i++){
+        arr2[i]=scan.nextInt();
+}   
+ortalama(arr2);
+} 
+}
